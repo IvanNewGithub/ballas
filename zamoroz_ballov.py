ee129e14c8012af0f6663305667cf4026cee986d
ivan <ballotelli@rambler.ru>
2024-01-29 16:31:17 +0400

    Соединил код и в маин запихнул

--- zamoroz_ballov.py
+++ zamoroz_ballov.py
@@ -19,13 +19,13 @@ def all_status():
     return mydictState
 
 
-def status(zakaz, state):
+def status(zakaz):
     headers2 = {
         "Authorization": f'Basic Z2FsY2V2QHNrbDRkbTpMaVRGcUlBTQ=='
     }
     data2 = {
         "state": {
-            all_status()[state]
+            all_status()['Доставлен']
         }}
     url2 = f"https://api.moysklad.ru/api/remap/1.2/entity/customerorder/{zakaz}"
     a3 = requests.put(url2, headers=headers2, json=data2)
@@ -40,7 +40,7 @@ def hours7(city, url, headers, params):  # Для саратова и балак
         data = req.json()
         for zakaz in data['rows']:
             if zakaz['organization']['name'] in city:
-                print(status(zakaz, state))
+                print(status(zakaz['id']))
         if data['rows'] < 100:
             break
 
@@ -53,18 +53,11 @@ def main(url, headers, params):
     }
     while True:
         current_time = datetime.now().time()  # текущее время
-        # current_time_msk = datetime.now().time()  # текущее время
-
-        if current_time.hour == 8 and current_time.minute == 0:
-            print("Время 8ч, выполняю задачу", f"-[{current_time}]-")
-            hours7(time_city[7], url, headers, params)
-            time.sleep(60)
-
-        if current_time.hour == 7 and current_time.minute == 0:
-            print("Время 9ч, выполняю задачу", f"-[{current_time}]-")
-            hours7(time_city[8], url, headers, params)
-            time.sleep(60)
-
+        for time_item in time_city:
+            if current_time.hour == time_item and current_time.minute == 0:
+                print(f"Время {time_item}ч, выполняю задачу", f"-[{current_time}]-")
+                hours7(time_city[time_item], url, headers, params)
+                time.sleep(60)
         if not current_time.hour in time_city and current_time.minute == 0:
             print("Начало часа, отписываюсь, время -", current_time)
 

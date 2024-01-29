6cf6d2f799fd573899f521a375f176342f0c2125
ivan <ballotelli@rambler.ru>
2024-01-29 16:19:18 +0400

    Соединил код и в маин запихнул

--- zamoroz_ballov.py
+++ zamoroz_ballov.py
@@ -4,20 +4,21 @@ import requests
 import time
 from datetime import datetime
 
-url = "https://api.moysklad.ru/api/remap/1.2/entity/customerorder"
-headers = {
-    "Authorization": f'Basic Z2FsY2V2QHNrbDRkbTpMaVRGcUlBTQ==',
-    "Accept-Encoding": "gzip"
-    }
 
-offset = 0
-state = "Доставлен (Без СМС)"
-params = {
-        "limit": 100,
-        "offset": offset,
-        "filter": f"state.name={state}",
-        "expand": "organization"
+def all_status():
+    headers2 = {
+        "Authorization": f'Basic Z2FsY2V2QHNrbDRkbTpMaVRGcUlBTQ=='
     }
+    url = "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata"
+    mydictState = {}
+    req = requests.get(url, headers=headers).json()
+    for item in req['states']:
+        mydictState[item['name']] = {}
+        mydictState[item['name']]['meta'] = item['meta']
+
+    return mydictState
+
+
 def status(zakaz, state):
     headers2 = {
         "Authorization": f'Basic Z2FsY2V2QHNrbDRkbTpMaVRGcUlBTQ=='
@@ -31,52 +32,58 @@ def status(zakaz, state):
     time.sleep(0.1)
     return a3.status_code
 
-def hours7(): # Для саратова и балаково
+
+def hours7(city, url, headers, params):  # Для саратова и балаково
     for offset in range(0, 1000, 100):
         state = "Доставлен (Без СМС)"
         req = requests.get(url, headers=headers, params=params)
         data = req.json()
         for zakaz in data['rows']:
-            if "саратов" in zakaz['organization']['name'] or "балаково" in zakaz['organization']['name']:
+            if zakaz['organization']['name'] in city:
                 print(status(zakaz, state))
         if data['rows'] < 100:
             break
 
-def hours8(): # Для липецка и Воронежа
-    pass
 
-def main():
+def main(url, headers, params):
+    time_city = {
+        7: ('саратов', 'балаково'),
+        8: ('воронеж', 'липецк')
+
+    }
     while True:
         current_time = datetime.now().time()  # текущее время
         # current_time_msk = datetime.now().time()  # текущее время
 
         if current_time.hour == 8 and current_time.minute == 0:
             print("Время 8ч, выполняю задачу", f"-[{current_time}]-")
-            hours7()
+            hours7(time_city[7], url, headers, params)
             time.sleep(60)
 
         if current_time.hour == 7 and current_time.minute == 0:
             print("Время 9ч, выполняю задачу", f"-[{current_time}]-")
-            hours8()
+            hours7(time_city[8], url, headers, params)
             time.sleep(60)
 
-        if not current_time.hour in (8, 7) and current_time.minute == 0:
+        if not current_time.hour in time_city and current_time.minute == 0:
             print("Начало часа, отписываюсь, время -", current_time)
 
         time.sleep(20)  # пауза в 20 секунд между проверками
 
-def all_status():
-    headers2 = {
-        "Authorization": f'Basic Z2FsY2V2QHNrbDRkbTpMaVRGcUlBTQ=='
+
+if __name__ == '__main__':
+    url = "https://api.moysklad.ru/api/remap/1.2/entity/customerorder"
+    headers = {
+        "Authorization": f'Basic Z2FsY2V2QHNrbDRkbTpMaVRGcUlBTQ==',
+        "Accept-Encoding": "gzip"
     }
-    url = "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata"
-    mydictState = {}
-    req = requests.get(url, headers=headers).json()
-    for item in req['states']:
 
-        print(item['name'])
-        mydictState[item['name']] = {}
-        mydictState[item['name']]['meta'] = item['meta']
-
-    return mydictState
-
+    offset = 0
+    state = "Доставлен (Без СМС)"
+    params = {
+        "limit": 100,
+        "offset": offset,
+        "filter": f"state.name={state}",
+        "expand": "organization"
+    }
+    main(url, headers, params)  # запуск основной функции

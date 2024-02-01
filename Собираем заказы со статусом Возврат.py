86708cfc15924cfc5b208d25b88e1ce843b5ade2
ivan <ballotelli@rambler.ru>
2024-02-01 08:44:02 +0300

    новое

--- Собираем заказы со статусом Возврат.py
+++ Собираем заказы со статусом Возврат.py
@@ -9,14 +9,28 @@ headers = {
 
 
 params = {
-        "limit": 10,
+        "limit": 100,
         "offset": 0,
         "filter": f"moment>2023-01-11 12:00:00;",
         'expand': 'demand,positions.assortment,agent'
     }
 
 url = "https://api.moysklad.ru/api/remap/1.2/entity/salesreturn"
-r1 = r.get(url, headers=headers, params=params).json()
+data = r.get(url, headers=headers, params=params).json()
+result = {}
+for x in data['rows']:
+    # print(x['phone'])
+    try:
+        result.setdefault(x['agent']['phone'], {})
+        result[x['agent']['phone']].setdefault('id заказа', []).append(x['id'])
+        result[x['agent']['phone']]['Внешний код товара'] = []
+        for j in x['positions']['rows']:
+            result[x['agent']['phone']]['Внешний код товара'].append(j['assortment']['externalCode'])
+            # result[x['agent']['phone']]['Внешний код товара'][j['assortment']['externalCode']] = j['things']
+    except:
+        pass
+
+
 with open('File/salereturn.json', 'w') as j:
-    json.dump(r1, j, ensure_ascii=False, indent=4)
+    json.dump(result, j, ensure_ascii=False, indent=4)
 # print(r1, r1.text)

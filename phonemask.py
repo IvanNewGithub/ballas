4b8f26ceb6b1c6a617c100cf94f00a672ea614d5
Aleksandr <103640972+Sanchezz007@users.noreply.github.com>
2024-02-01 13:59:44 +0300

    Маска телефона

--- phonemask.py
+++ phonemask.py
@@ -1,20 +1,22 @@
 import re
 
-def phone_check():
-    phone = input("Введите номер: ")
+def phone_check(phone):
     phones_mask = re.sub(r'\D', '', phone)
 
     print(f"\"{phones_mask}\"")
 
     if len(phones_mask) == 11:
         if int(phones_mask[0]) == 7 or int(phones_mask[0]) == 8:
-            print("Проверка телефона по маске2: Все ОК")
+            if int(phones_mask[0]) == 8:
+                phones_mask = "7"+phones_mask[1:]
+                return phones_mask
             return phones_mask
         else:
             print("Номер телефона начинается не с 8 или 7, при этом кол-во цифр равно 11")
             return 0
     elif len(phones_mask) == 10:
         if int(phones_mask[0]) == (9):
+            phones_mask = "7" + phones_mask
             print("Проверка телефона по маске: Все ОК, забыли прописать 7 или 8")
             return phones_mask
         else:
@@ -26,3 +28,7 @@ def phone_check():
     elif len(phones_mask) < 10:
         print("Номер телефона содержит менее 10 цифр, проверьте номер телефона и попробуйте снова")
         return 0
+
+phone = "9525959117"
+a1 = phone_check(phone)
+print(a1)

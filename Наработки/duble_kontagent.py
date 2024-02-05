9f49b0bde8cc5f921850db2d284c4c0fbc76936f
ivan <ballotelli@rambler.ru>
2024-02-05 10:21:06 +0300

    новое

--- Наработки/duble_kontagent.py
+++ Наработки/duble_kontagent.py
@@ -1,3 +1,5 @@
+import json
+
 import googleapiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
@@ -29,9 +31,35 @@ class google_requst:
         simall.columns = simall.iloc[0]
         return simall
 
+def obrabotka(current_tel):
+    result = {}
+    for index, item in current_tel.T.items():
+        if item['есть основной?']:
+            if item['phones'] not in result:
+                result[item['phones']] = {}
+            result[item['phones']]['osnova'] = item['id']
+        else:
+            if item['phones'] not in result:
+                result[item['phones']] = {}
+            if 'dubl' not in result[item['phones']]:
+                result[item['phones']]['dubl'] = [item['id']]
+            else:
+                result[item['phones']]['dubl'].append(item['id'])
+    return result
 
-id_table = '1V0thoYAF4quDw2bn2TZDv8mmOYLTW_kH0SkmFCqW4RQ'
-range = 'cust_file'
 
-data = google_requst(id_table, range).download_table()
-print(data)
+if __name__ == '__main__':
+    id_table = '1V0thoYAF4quDw2bn2TZDv8mmOYLTW_kH0SkmFCqW4RQ'
+    range = 'cust_file'
+    data = google_requst(id_table, range).download_table()
+    # print(data['есть основной?'])
+    df = data.replace('#N/A', None)
+    contagent_MS = list(df.dropna(axis='index', how='any', subset=['есть основной?']))
+    # contagent_MS.to_csv('../File/contagent.csv')
+    # print(contagent_MS)
+    current_tel = pd.DataFrame(df.loc[df['phones'].str.len() >= 11]) # Собираем только те строки, где длина номера больше или равна 11
+    print(type(current_tel))
+    current_tel.to_csv('../File/contagent.csv')
+    with open('../File/dubl.json', 'w') as j:
+        json.dump(obrabotka(current_tel), j, ensure_ascii=False, indent=4)
+    print('Comlete!')
